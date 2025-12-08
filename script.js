9b5945c8aa82c319728927d244c9baa645f01168
rubyyun0709-star <rubyyun0709@gmail.com>
2025-12-08 16:05:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const buttons = document.querySelectorAll('.experience-btn');
+ <script>
+    const entryImages = document.querySelectorAll(".entry img");
 
-buttons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const img = btn.parentElement.querySelector('img');
-    const whiteSrc = btn.dataset.white;
-    const colorSrc = btn.dataset.color;
-
-    // 원래 이미지로 돌아오는 기능 추가
-    img.src = colorSrc; // 클릭 즉시 컬러 이미지
-    setTimeout(() => {
-      img.src = whiteSrc; // 3초 후 원래 흑백 이미지로
-    }, 2000);
-  });
-});
+    entryImages.forEach(img => {
+      img.addEventListener("dblclick", () => {
+        const target = img.dataset.target;
+        if(target) {
+          window.location.href = target;
+        }
+      });
+    });
+  </script>
